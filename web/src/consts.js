dbf5ed889d560b47ee6e81526a2321d7b3e00e34
Angel <angelbeneman801@gmail.com>
2024-08-07 13:20:20 -0300

    updated

---
+++ web/src/consts.js
@@ -0,0 +1,30 @@
+export const PROJECTS = [
+  {
+    slug: "01-tinder-swipe",
+    title: "Tinder Swipe",
+    hidden: false,
+    description:
+      "Aprende a crear el efecto de swipe de Tinder con HTML, CSS y JavaScript.",
+    learnings: [
+      "DOM",
+      "Animaciones con gestos",
+      "Eventos de mouse y touch",
+      "Transiciones CSS",
+    ],
+    blog: 'https://angeldev-blog.vercel.app/blog'
+  },
+  {
+    slug: "02-tetris-canvas",
+    title: "Tetris en Canvas",
+    description: "Resolvemos una prueba técnica que te propone crear el Tetris en 40 minutos",
+    learnings: [
+      "Canvas",
+      "Eventos de teclado",
+      "Lógica de programación"
+    ],
+    theme: {
+      isDark: true
+    },
+    blog: "https://angeldev-blog.vercel.app/blog"
+  },
+]
