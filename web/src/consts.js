7b6b5bd67fdc7d5ae01037caf1d2749d303fe5fa
Angel <angelbeneman801@gmail.com>
2024-08-12 17:26:35 -0300

    Updated

--- web/src/consts.js
+++ web/src/consts.js
@@ -27,4 +27,18 @@ export const PROJECTS = [
     },
     blog: "https://angeldev-blog.vercel.app/blog"
   },
+  {
+    slug: "03-tetris-canvas",
+    title: "Tetris en Canvas",
+    description: "Resolvemos una prueba técnica que te propone crear el Tetris en 40 minutos",
+    learnings: [
+      "Canvas",
+      "Eventos de teclado",
+      "Lógica de programación"
+    ],
+    theme: {
+      isDark: true
+    },
+    blog: "https://angeldev-blog.vercel.app/blog"
+  },
 ]
