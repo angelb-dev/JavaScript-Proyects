786e10facd6145147a25c98436f2e4be8ab62b2b
Angel <angelbeneman801@gmail.com>
2024-09-24 15:25:12 -0300

    Updated

--- web/src/consts.js
+++ web/src/consts.js
@@ -1,7 +1,7 @@
 export const PROJECTS = [
   {
-    slug: '01-calculator',
-    title: 'Calculator',
+    slug: '01-wild-rift-stats',
+    title: 'Wild Rift Stats',
     hidden: false,
     description:
       'Desarrolla una calculadora interactiva utilizando HTML, CSS y JavaScript.',
@@ -15,8 +15,8 @@ export const PROJECTS = [
     blog: 'https://angeldev-blog.vercel.app/blog'
   },
   {
-    slug: '02-contador-de-numero-par',
-    title: 'Contador de Números Pares',
+    slug: '02-chronometer',
+    title: 'Chonometer',
     description: 'Crea un contador interactivo de números pares utilizando JavaScript.',
     learnings: [
       'DOM',
@@ -29,8 +29,8 @@ export const PROJECTS = [
     blog: 'https://angeldev-blog.vercel.app/blog'
   },
   {
-    slug: '03-tinder-swipe',
-    title: 'Tinder Swipe',
+    slug: '03-even-number-counter',
+    title: 'Even number counter',
     hidden: false,
     description:
       'Aprende a crear el efecto de swipe de Tinder con HTML, CSS y JavaScript.',
@@ -43,8 +43,78 @@ export const PROJECTS = [
     blog: 'https://angeldev-blog.vercel.app/blog'
   },
   {
-    slug: '04-tetris-canvas',
-    title: 'Tetris en Canvas',
+    slug: '04-rpg-tasks',
+    title: 'RPG Tasks',
+    description: 'Resolvemos una prueba técnica que te propone crear el Tetris en 40 minutos',
+    learnings: [
+      'Canvas',
+      'Eventos de teclado',
+      'Lógica de programación'
+    ],
+    theme: {
+      isDark: true
+    },
+    blog: 'https://angeldev-blog.vercel.app/blog'
+  },
+  {
+    slug: '05-calculator',
+    title: 'Calculator',
+    description: 'Resolvemos una prueba técnica que te propone crear el Tetris en 40 minutos',
+    learnings: [
+      'Canvas',
+      'Eventos de teclado',
+      'Lógica de programación'
+    ],
+    theme: {
+      isDark: true
+    },
+    blog: 'https://angeldev-blog.vercel.app/blog'
+  },
+  {
+    slug: '06-tinder-swiper',
+    title: 'Tinder Swiper',
+    description: 'Resolvemos una prueba técnica que te propone crear el Tetris en 40 minutos',
+    learnings: [
+      'Canvas',
+      'Eventos de teclado',
+      'Lógica de programación'
+    ],
+    theme: {
+      isDark: true
+    },
+    blog: 'https://angeldev-blog.vercel.app/blog'
+  },
+  {
+    slug: '07-numberdown',
+    title: 'Numberdown',
+    description: 'Resolvemos una prueba técnica que te propone crear el Tetris en 40 minutos',
+    learnings: [
+      'Canvas',
+      'Eventos de teclado',
+      'Lógica de programación'
+    ],
+    theme: {
+      isDark: true
+    },
+    blog: 'https://angeldev-blog.vercel.app/blog'
+  },
+  {
+    slug: '08-tetris-canvas',
+    title: 'Tetris Canvas',
+    description: 'Resolvemos una prueba técnica que te propone crear el Tetris en 40 minutos',
+    learnings: [
+      'Canvas',
+      'Eventos de teclado',
+      'Lógica de programación'
+    ],
+    theme: {
+      isDark: true
+    },
+    blog: 'https://angeldev-blog.vercel.app/blog'
+  },
+  {
+    slug: '09-infinite-tic-tac-toe',
+    title: 'Infinite Tic Tac Toe',
     description: 'Resolvemos una prueba técnica que te propone crear el Tetris en 40 minutos',
     learnings: [
       'Canvas',
